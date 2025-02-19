1e03b5eb8d50984d79114c7e62b7a8ad4c342956
krish30052006 <152592411+krish30052006@users.noreply.github.com>
2025-02-19 23:50:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.get('/download-log', (req, res) => {
+    const logPath = path.join(__dirname, 'visits.log');
+    res.download(logPath, 'visits.log', (err) => {
+        if (err) {
+            res.status(500).send("Error downloading file");
+        }
+    });
+});
+
+app.use((req, res, next) => {
+    const logEntry = `${new Date().toISOString()} - ${req.ip}\n`;
+    fs.appendFileSync('visits.log', logEntry);
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/logs', (req, res) => {
+    fs.readFile('visits.log', 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Could not read log file' });
+        }
+        res.json({ logs: data.split('\n').filter(line => line) });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
